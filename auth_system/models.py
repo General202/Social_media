38c03ddd946849bcf32112d6fcb530e8bcf4cf66
General202 <user.den.ua@gmail.com>
2025-09-16 21:01:14 +0300

    Implement custom user model and initial migration for auth_system

--- auth_system/models.py
+++ auth_system/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+class CustomUser(AbstractUser):
+    birsth_date = models.DateField(null=True, blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+    cover_image = models.ImageField(upload_to='cover_images/', null=True, blank=True)
+    profile_image = models.ImageField(upload_to='profile_images/', null=True, blank=True)
+    
